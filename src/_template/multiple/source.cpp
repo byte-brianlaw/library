6dba0a8bf73c7be327f1ee8e7a022343c25a3119
Brian Law <byte.brianlaw@gmail.com>
2022-11-06 21:51:25 -0800

    Add base templates
    - Add single template
    - Add multiple template

---
+++ src/_template/multiple/source.cpp
@@ -0,0 +1,28 @@
+#include <ios>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::ios_base;
+
+auto solve() {
+
+}
+
+int main() {
+
+    cin.tie(nullptr);
+
+    ios_base::sync_with_stdio(false);
+
+    auto t = 0;
+
+    cin >> t;
+
+    for (auto i = 1; i <= t; ++i) {
+        solve();
+    }
+
+    return 0;
+
+}
---
+++ src/_template/single/source.cpp
@@ -0,0 +1,22 @@
+#include <ios>
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::ios_base;
+
+auto solve() {
+
+}
+
+int main() {
+
+    cin.tie(nullptr);
+
+    ios_base::sync_with_stdio(false);
+
+    solve();
+
+    return 0;
+
+}
