126c387a0d7bf27673b789592350de840b850efb
Brian Law <byte.brianlaw@gmail.com>
2022-12-04 04:02:09 -0800

    Add `IntegralHash`
    - Add `IntegralHash`

---
+++ src/hash/integral.cpp
@@ -0,0 +1,26 @@
+#include <chrono>
+#include <cstddef>
+
+template <typename T>
+class IntegralHash {
+
+public:
+
+    auto operator()(T value) const {
+
+        static const auto constant = static_cast<unsigned long long>(
+            std::chrono::steady_clock::now().time_since_epoch().count()
+        );
+
+        auto result = static_cast<unsigned long long>(value);
+
+        result += constant + 0x9e3779b97f4a7c15ll;
+        result = (result ^ (result >> 30)) * 0xbf58476d1ce4e5b9ll;
+        result = (result ^ (result >> 27)) * 0x94d049bb133111ebll;
+        result ^= result >> 31;
+
+        return static_cast<std::size_t>(result);
+
+    }
+
+};
