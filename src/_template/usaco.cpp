bf553f51b74563535ef8c2435fd260801a6c51cd
Brian Law <byte.brianlaw@gmail.com>
2022-12-04 03:16:10 -0800

    Refactor templates
    - Add USACO template
    - Move multiple template
    - Move single template

---
+++ src/_template/usaco.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+
+using std::ifstream;
+using std::ofstream;
+
+auto fin = ifstream(".in");
+auto fout = ofstream(".out");
+
+auto solve() {
+
+}
+
+int main() {
+
+    solve();
+
+    return 0;
+
+}
